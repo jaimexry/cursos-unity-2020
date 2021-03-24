71524f186dd483fa31a2a37d6662dcc27e4e93f7
jaimexry <jaimexry@gmail.com>
2021-03-24 15:32:00 +0100

    Traget Script(Project 5)

--- 05_QuickClick/Assets/_Scripts/Target.cs
+++ 05_QuickClick/Assets/_Scripts/Target.cs
@@ -4,12 +4,52 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+
+    private Rigidbody _rigidbody;
+
+    private float minForce = 15,
+        maxForce = 17,
+        maxTorque = 10,
+        xRange = 4,
+        ySpawnPos = -6;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(), RandomTorque(),
+                             RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPosition();
     }
 
+    /// <summary>
+    /// Genera un Vector3 aleatorio en 3D
+    /// </summary>
+    /// <returns>Fuerza aleatoria hacia arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+
+    /// <summary>
+    /// Genera un número Float aleatorio
+    /// </summary>
+    /// <returns>Valor aleatorio entre -maxTorque y maxTorque</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    
+    /// <summary>
+    /// Genera una posición aleatoria
+    /// </summary>
+    /// <returns>Posición aleatoria en 3D, con la coordenada z = 0</returns>
+    private Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+    
     // Update is called once per frame
     void Update()
     {
