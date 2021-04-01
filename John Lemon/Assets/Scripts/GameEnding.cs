1a663dfa6df9120ce9e96307aa36b67adc751942
jaimexry <jaimexry@gmail.com>
2021-04-01 16:23:40 +0200

    John Lemon Gameplay

---
+++ John Lemon/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    
+    private bool isPlayerAtExit;
+    private bool isPlayerCaught;
+    public GameObject player;
+
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    
+    private float timer;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerAtExit && !isPlayerCaught)
+        {
+           EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if (isPlayerCaught && !isPlayerAtExit)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+    
+    /// <summary>
+    /// Lanza la imagen de fin de la partida y cierra el juego
+    /// </summary>
+    /// <param name="image">Imagen de fin de partida correspondiente</param>
+    /// <param name="doRestart">Parámetro de reinicio de partida</param>
+    private void EndLevel(CanvasGroup image, bool doRestart)
+    {
+        timer += Time.deltaTime;
+        image.alpha = timer / fadeDuration;
+
+        if (timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void CatchPlayer()
+    {
+        isPlayerCaught = true;
+    }
+}
---
+++ John Lemon/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class WaypointPatrol : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+
+    public Transform[] waypoints;
+    private int currentWaypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_navMeshAgent.remainingDistance < _navMeshAgent.stoppingDistance)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            _navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+}
