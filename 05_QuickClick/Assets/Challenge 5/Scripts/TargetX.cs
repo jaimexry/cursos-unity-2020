a41bb6e289e01fdda111c24440b53208d1f36672
jaimexry <jaimexry@gmail.com>
2021-04-01 16:33:30 +0200

    John Lemon Gameplay Implemented

---
+++ 05_QuickClick/Assets/Challenge 5/Scripts/TargetX.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetX : MonoBehaviour
+{
+    private GameManagerX gameManagerX;
+    public int pointValue;
+    public GameObject explosionFx;
+
+    public float timeOnScreen = 1.0f;
+    void Start()
+    {
+        gameManagerX = FindObjectOfType<GameManagerX>();
+
+        StartCoroutine(RemoveObjectRoutine()); // begin timer before target leaves screen
+    }
+
+    // When target is clicked, destroy it, update score, and generate explosion
+    private void OnMouseDown()
+    {
+        if (gameManagerX.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManagerX.UpdateScore(pointValue);
+            Explode();
+        }
+               
+    }
+
+    // If target that is NOT the bad object collides with sensor, trigger game over
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if (other.gameObject.CompareTag("Sensor") && !gameObject.CompareTag("Bad"))
+        {
+            gameManagerX.GameOver();
+        } 
+
+    }
+
+    // Display explosion particle at object's position
+    void Explode ()
+    {
+        Instantiate(explosionFx, transform.position, explosionFx.transform.rotation);
+    }
+
+    // After a delay, Moves the object behind background so it collides with the Sensor object
+    IEnumerator RemoveObjectRoutine ()
+    {
+        yield return new WaitForSeconds(timeOnScreen);
+        if (gameManagerX.isGameActive)
+        {
+            transform.Translate(Vector3.forward * 5, Space.World);
+        }
+
+    }
+
+}
---
+++ John Lemon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Quaternion rotation = Quaternion.identity;
+    private Vector3 movement;
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+    
+    [SerializeField]
+    private float turnSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement.Set(horizontal, 0, vertical);
+        movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        
+        _animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward,
+            movement, turnSpeed * Time.fixedDeltaTime, 0f);
+        
+        rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
